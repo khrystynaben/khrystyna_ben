14a1c19c93b0858593bfe9187c1c7d32b8b26b63
khrystynaben <90345579+khrystynaben@users.noreply.github.com>
2021-09-16 11:27:10 +0300

    Update programming1.py

--- programming1.py
+++ programming1.py
@@ -1,51 +1,46 @@
 import sys
 
 
-def left_cyclic_shift1(list_, k):
-    for i in range(k):
-        list_ = list_[1:] + [list_[0]]
-    return list_
-
-
-def left_cyclic_shift2(list_, k):
+def left_cyclic_shift(list_, k):
     k = k % len(list_)
     return list_[k:] + list_[:k]
 
 
-def left_cyclic_shift3(list_, k):
-    for i in range(k):
-        list_.append(list_.pop(0))
-    return list_
-
-
-def validate(x):
+def integer_checking(x):
     try:
-        n = int(x)
+        temp_var = int(x)
     except:
         print('must be integer')
         sys.exit()
-    if n < 0:
-        print('must be positive')
-        sys.exit()
-    return n
+    return temp_var
 
-array = []
+
+def full_checking(x, check_index):
+    if check_index == 1:
+       if integer_checking(x) < 0:
+            print('must be positive')
+            sys.exit()
+       else:
+           return integer_checking(x)
+
+    else:
+        return integer_checking(x)
+
+
+int_array = []
 quantity = input("Input quantity of the list: ")
-quantity = validate(quantity)
+quantity = full_checking(quantity, 1)
 
 for i in range(quantity):
-    array.append(input(f"Input element {i+1}: "))
-print(f"initial list: {array}")
+    element = (input(f"Input element {i+1}: "))
+    element = full_checking(element, 2)
+    int_array.append(element)
+
+print(f"initial list: {int_array}")
 
 k = input("Input k: ")
-k = validate(k)
+k = full_checking(k, 1)
 
 
 print("final list: ")
-i = 1
-for func in [left_cyclic_shift1, left_cyclic_shift2, left_cyclic_shift3]:
-    print(f'method {i}: {func(array, k)}')
-    i += 1
-
-
-
+print(left_cyclic_shift(int_array, k))
