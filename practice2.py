e4c29edeeecb72d1bf510d5c1e5e25aa097b89d9
khrystynaben <90345579+khrystynaben@users.noreply.github.com>
2021-09-28 23:48:22 +0300

    Update practice2.py

--- practice2.py
+++ practice2.py
@@ -4,35 +4,32 @@ import sys
 
 operations = 0
 
-def integer_checking(x):
+
+def show_error(text):
+    print(text)
+    sys.exit()
+
+
+def try_check(x, type_x):
     try:
-        temp_var = int(x)
+        temp_var = type_x(x)
     except:
-        print('must be integer')
-        sys.exit()
+        show_error(f"must be {str(type_x).split(chr(39))[-2]}")
     return temp_var
 
 
-def full_checking(x, type_check):
-    if type_check == 'int and posit':
-        temp_var = integer_checking(x)
-        if temp_var <= 0:
-            print('must be positive')
-            sys.exit()
+def full_checking(x, type_check, positive=False):
+    if type_check is int:
+        temp_var = try_check(x, int)
+        if positive == True and temp_var < 0:
+            show_error('must be positive')
         return temp_var
-    elif type_check == '1/2':
-        temp_var = integer_checking(x)
-        if (temp_var == 1 or temp_var == 2) == False:
-            print('must be 1 or 2')
-            sys.exit()
-        return temp_var
-    else:
-        try:
-            temp_var = float(x)
-        except:
-            print('must be number')
-            sys.exit()
-        return temp_var
+    elif type(type_check) is list:
+        if x not in type_check:
+            show_error('incorrect option')
+        return x
+    elif type_check is float:
+         return try_check(x, float)
 
 
 def merge_sort(list_):
@@ -71,37 +68,55 @@ def merge_sort(list_):
     return list_
 
 
-while True:
-    n = input("n = ")
-    n = full_checking(n, 'int and posit')
+def generation(n_):
+    a = input("a = ")
+    a = full_checking(a, float)
+    b = input("b = ")
+    b = full_checking(b, float)
+    if a >= b:
+        print("a must be less then b")
+        sys.exit()
+    print("Input 1 if you choose integer ")
+    print("Input 2 if you choose for float")
+    type_option = input('? ')
+    type_option = full_checking(type_option, ['1','2'])
+    if type_option == '1':
+        array_ = [randint(a, b) for _ in range(n_)]
+    else:
+        array_ = [uniform(a, b) for _ in range(n_)]
+    return array_
+
+
+def self_inputting():
+    array_ = []
+    for i in range(n):
+        element = input(f"Input element {i + 1}: ")
+        element = full_checking(element, float)
+        array_.append(element)
+    return array_
+
+
+def inputting(n_):
     print("Input 1 for creating list step by step")
     print("Input 2 for generate list automatically")
     input_option = input("? ")
-    input_option = full_checking(input_option, '1/2')
-    if input_option == 1:
-        array = []
-        for i in range(n):
-            element = input(f"Input element {i + 1}: ")
-            element = full_checking(element, 'other')
-            array.append(element)
+    input_option = full_checking(input_option, ['1', '2'])
+    if input_option == '1':
+        array = self_inputting()
     else:
-        a = input("a = ")
-        a = full_checking(a, 'other')
-        b = input("b = ")
-        b = full_checking(b, 'other')
-        print("Input 1 if you choose integer ")
-        print("Input 2 if you choose for float")
-        type_option = input('? ')
-        type_option = full_checking(type_option, '1/2')
-        if type_option == 1:
-            array = [randint(a, b) for _ in range(n)]
-        else:
-            array = [uniform(a, b) for _ in range(n)]
+        array = generation(n_)
+    return array
+
+
+while True:
+    n = input("n = ")
+    n = full_checking(n, int, positive=True)
+    array = inputting(n)
     print(f"Initial list: {array}")
     merge_sort(array)
     print(f"After sorting: {array}")
     print("Number of operations: ", operations)
     do_next = input("1 - next list; 2 - exit\n? ")
-    do_next = full_checking(do_next, '1/2')
-    if do_next == 2:
+    do_next = full_checking(do_next, ['1', '2'])
+    if do_next == '2':
         break
