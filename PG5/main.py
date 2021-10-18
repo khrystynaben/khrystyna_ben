12a3ac2fad1fa8ed2fc5429eb882416b6384faa1
khrystynaben <90345579+khrystynaben@users.noreply.github.com>
2021-10-18 23:44:22 +0300

    Create main.py

---
+++ PG5/main.py
@@ -0,0 +1,21 @@
+from os.path import exists
+from pr import PaymentRequest
+from collect import Collection
+import sys
+from valid import *
+from menu import *
+import re
+
+
+c = Collection('dani.txt')
+print(c)
+
+
+while True:
+    menu(c)
+    do_next = input("1 - next; 2 - exit\n? ")
+    do_next = validate_specific_elem(do_next, ['1', '2'])
+    if do_next == '2':
+        break
+
+
