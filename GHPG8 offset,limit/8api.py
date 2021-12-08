32add5741cd7c47c0ddec22ca73ad554e0c9292d
khrystynaben <90345579+khrystynaben@users.noreply.github.com>
2021-12-08 23:53:11 +0200

    Update 8api.py

--- GHPG8 offset,limit/8api.py
+++ GHPG8 offset,limit/8api.py
@@ -4,11 +4,16 @@ from flask_marshmallow import Marshmallow
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:1234@localhost:5432/payment_request"
-#app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.environ['USER']}:{os.environ['PASSWORD']}@{os.environ['HOST']}/{os.environ['DATABASE']}"
+#app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:1234@localhost:5432/payment_request"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.environ['USER']}:{os.environ['PASSWORD']}@{os.environ['HOST']}/{os.environ['DATABASE']}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+
+
+
+
 
 
 
